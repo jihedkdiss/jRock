cf6bb893a37a793b63d2d81d75367615448e74f0
Jihed Kdiss <jihedkdiss@outlook.com>
2022-03-17 13:55:05 +0100

    Added choices and input
    This will print the available choices and wait for the user to choose one.

--- src/jrock/Main.java
+++ src/jrock/Main.java
@@ -1,10 +1,19 @@
 package jrock;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("+------------------------------+");
         System.out.println("| === jRock by Jihed Kdiss === |");
         System.out.println("+------------------------------+");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please choose:");
+        System.out.println("1. Rock");
+        System.out.println("2. Paper");
+        System.out.println("3. Scissors");
+        System.out.print("  > ");
+        int choice = input.nextInt();
     }
 }
