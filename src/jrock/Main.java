9a7a2b6dcaac08cda7c4238e07f4aec70fec1376
Jihed Kdiss <jihedkdiss@outlook.com>
2022-03-17 14:37:12 +0100

    :star: Add formatting
    Make sure everything looks good and polished!

--- src/jrock/Main.java
+++ src/jrock/Main.java
@@ -9,35 +9,40 @@ public class Main {
         System.out.println("| === jRock by Jihed Kdiss === |");
         System.out.println("+------------------------------+");
         Scanner input = new Scanner(System.in);
-        System.out.println("Please choose:");
-        System.out.println("1. Rock");
-        System.out.println("2. Paper");
-        System.out.println("3. Scissors");
-        System.out.print("  > ");
+        System.out.println("  Please choose:");
+        System.out.println("+------------------------------+");
+        System.out.println("  1. Rock");
+        System.out.println("  2. Paper");
+        System.out.println("  3. Scissors");
+        System.out.println("+------------------------------+");
+        System.out.print("  Your choice: ");
         int choice = input.nextInt();
+        System.out.println("+------------------------------+");
         int computerChoice = (int)(Math.random() * 10) % 3;
         computerChoice++;
         String[] options = {"@jihedkdiss", "Rock", "Paper", "Scissors"};
-        System.out.println("Computer choice is: " + options[computerChoice]);
+        System.out.println("  Computer choice is: " + options[computerChoice]);
         if (choice == computerChoice) {
-            System.out.println("No one wins!");
+            System.out.println("  No one wins!");
+            System.out.println("+------------------------------+");
             System.exit(0);
         }
         switch (choice) {
             case 1:
-                if(computerChoice == 2) System.out.println("You lose!");
-                else System.out.println("You win!");
+                if(computerChoice == 2) System.out.println("  You lose!");
+                else System.out.println("  You win!");
                 break;
             case 2:
-                if(computerChoice == 3) System.out.println("You lose!");
-                else System.out.println("You win!");
+                if(computerChoice == 3) System.out.println("  You lose!");
+                else System.out.println("  You win!");
                 break;
             case 3:
-                if(computerChoice == 1) System.out.println("You lose!");
-                else System.out.println("You win!");
+                if(computerChoice == 1) System.out.println("  You lose!");
+                else System.out.println("  You win!");
                 break;
             default:
-                System.out.println("Please choose correctly.");
+                System.out.println("  Please choose correctly.");
         }
+        System.out.println("+------------------------------+");
     }
 }
